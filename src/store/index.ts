29f826a61cb64f8c1a66b2f34f7b19bb372d8a71
Potapov <potapov.eo@gmail.com>
2022-05-29 18:36:37 +0300

    отображение

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+import weather from './modules/weather.module'
+
+export default createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    weather
+  }
+})
