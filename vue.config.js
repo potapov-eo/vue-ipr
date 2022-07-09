63fb9b5d57eb6ac00cd37cf4003bb02593b3453f
Potapov <potapov.eo@gmail.com>
2022-07-09 22:24:36 +0300

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
- // publicPath: '/',
-  lintOnSave: false
-})
+
+module.exports = {
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
+}
